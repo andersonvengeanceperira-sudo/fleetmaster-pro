17054ee5a80ae1f492fb8e1372ad7b7459d27e2c
andersonvengeanceperira-sudo <andersonvengeanceperira@gmail.com>
2026-03-06 08:48:53 -0300

    Add basic Express server setup

---
+++ frontend/js/frontend/frontend/js/frontend/css/backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+
+res.send("FleetMaster API")
+
+})
+
+app.listen(3000,()=>{
+
+console.log("Servidor rodando")
+
+})
