e90e36cb710c82fe41f5d252fdccb082daf2508e
andersonvengeanceperira-sudo <andersonvengeanceperira@gmail.com>
2026-03-06 08:46:55 -0300

    Add user registration function to register.js

---
+++ frontend/js/register.js
@@ -0,0 +1,23 @@
+function registrar(){
+
+let nome = document.getElementById("nome").value
+let email = document.getElementById("email").value
+let senha = document.getElementById("senha").value
+
+let usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]")
+
+usuarios.push({
+
+nome,
+email,
+senha
+
+})
+
+localStorage.setItem("usuarios",JSON.stringify(usuarios))
+
+alert("Usuário cadastrado")
+
+window.location="index.html"
+
+}
